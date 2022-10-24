3d1a019cbc4c64128865358925a602d9849d150b
shihab-sabbir <shihab11231@gmail.com>
2022-10-25 01:37:55 +0600

    pdf dome

--- src/component/Hotel/Hotel.jsx
+++ src/component/Hotel/Hotel.jsx
@@ -3,42 +3,40 @@ import React, { useContext, useRef } from 'react'
 import { Link, useLoaderData, useNavigate, useOutletContext } from 'react-router-dom';
 import { AuthContext } from '../../UserContext/UserContext';
 import { BsDownload } from 'react-icons/bs';
-import * as jsPDF from 'jspdf';
-import * as html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+import logo from '../../asset/logo.png'
+import replacement from '../../asset/replacement.png'
 function Hotel() {
     const data = useLoaderData();
     const { name, image, cost, details } = data;
     const { isMenuOpen } = useOutletContext();
     const navigate = useNavigate();
-    const doc = new jsPDF();
+    const doc = new jsPDF('p', 'px', 'letter');
     const input = useRef()
     function handleOnclick() {
         navigate('/booking');
     }
-
-    function handleDownload() {
-        // doc.text(name, 10, 10);
-        // doc.text(details, 10, 10);
-        // doc.text(`${cost}`, 10, 10);
-        // doc.addImage(image, 'PNG', 15, 40, 200, 114);
-        // doc.output('datauri');
-        // doc.save(`${name}.pdf`);
-        html2canvas(input.current)
-            .then((canvas) => {
-                const imgData = canvas.toDataURL(image);
-                const pdf = new jsPDF('p', 'px', 'a4');
-                var width = pdf.internal.pageSize.getWidth();
-                var height = pdf.internal.pageSize.getHeight();
-                pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
-                pdf.save(`${name}.pdf`);
-            });
+    const errImg = useRef();
+    const replaceImage = () => {
+        errImg.current.src = replacement;
     }
-
+    function handleDownload() {
+        doc.setFontSize(12);
+        doc.setLineHeightFactor(1.5)
+        doc.addImage(logo, 'PNG', 180, 20, 100, 40);
+        doc.addImage(replacement, 'PNG', 100, 90, 250, 200);
+        doc.text(name, 20, 320);
+        doc.text(`Course Details : ${details}`, 20, 350, { maxWidth: 450 });
+        doc.text(`Price : $${cost}`, 20, 400);
+        doc.text('Thank for visiting us !', 20, 440);
+        doc.output('datauri');
+        doc.save(`${name}.pdf`);
+    }
     return (
         <div className={isMenuOpen ? 'pt-[270px] mb-3 px-3 min-h-screen' : 'mb-3 p-3 min-h-screen'}>
             <div className="hero min-h-screen bg-base-200 lg:max-w-[1100px] mx-auto" ref={input}>
                 <div className="hero-content flex flex-col md:flex-row" >
-                    <img src={image} className="max-w-full md:min-w-[50%] shadow md:max-h-[500px]" />
+                    <img src={image} ref={errImg} onError={() => { replaceImage() }} className="max-w-full md:min-w-[50%] shadow md:max-h-[500px]" />
                     <div className='flex flex-col justify-between items-start min-w-full md:min-w-[50%] px-3 pb-1 md:pt-0 pt-5 md:max-h-[500px]'>
                         <div className='flex justify-between gap-2'>
                             <h1 className="text-2xl font-bold">{name}</h1>
