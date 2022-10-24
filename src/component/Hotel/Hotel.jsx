d3ef028839c040a941baaafeeda3188d294e89bc
shihab-sabbir <shihab11231@gmail.com>
2022-10-25 00:59:43 +0600

    route created

--- src/component/Hotel/Hotel.jsx
+++ src/component/Hotel/Hotel.jsx
@@ -1,33 +1,43 @@
 import { Button, Tooltip } from 'flowbite-react';
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { Link, useLoaderData, useNavigate, useOutletContext } from 'react-router-dom';
 import { AuthContext } from '../../UserContext/UserContext';
 import { BsDownload } from 'react-icons/bs';
-import { jsPDF } from "jspdf";
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 function Hotel() {
     const data = useLoaderData();
     const { name, image, cost, details } = data;
     const { isMenuOpen } = useOutletContext();
     const navigate = useNavigate();
     const doc = new jsPDF();
-
+    const input = useRef()
     function handleOnclick() {
         navigate('/booking');
     }
 
     function handleDownload() {
-        doc.text(name, 10, 10);
-        doc.text(details, 10, 10);
-        doc.text(`${cost}`, 10, 10);
-        doc.addImage(image, 'PNG', 15, 40, 200, 114);
-        doc.output('datauri');
-        doc.save(`${name}.pdf`);
+        // doc.text(name, 10, 10);
+        // doc.text(details, 10, 10);
+        // doc.text(`${cost}`, 10, 10);
+        // doc.addImage(image, 'PNG', 15, 40, 200, 114);
+        // doc.output('datauri');
+        // doc.save(`${name}.pdf`);
+        html2canvas(input.current)
+            .then((canvas) => {
+                const imgData = canvas.toDataURL(image);
+                const pdf = new jsPDF('p', 'px', 'a4');
+                var width = pdf.internal.pageSize.getWidth();
+                var height = pdf.internal.pageSize.getHeight();
+                pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
+                pdf.save(`${name}.pdf`);
+            });
     }
 
     return (
         <div className={isMenuOpen ? 'pt-[270px] mb-3 px-3 min-h-screen' : 'mb-3 p-3 min-h-screen'}>
-            <div className="hero min-h-screen bg-base-200 lg:max-w-[1100px] mx-auto">
-                <div className="hero-content flex flex-col md:flex-row">
+            <div className="hero min-h-screen bg-base-200 lg:max-w-[1100px] mx-auto" ref={input}>
+                <div className="hero-content flex flex-col md:flex-row" >
                     <img src={image} className="max-w-full md:min-w-[50%] shadow md:max-h-[500px]" />
                     <div className='flex flex-col justify-between items-start min-w-full md:min-w-[50%] px-3 pb-1 md:pt-0 pt-5 md:max-h-[500px]'>
                         <div className='flex justify-between gap-2'>
